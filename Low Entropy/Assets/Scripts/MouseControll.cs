324b16807384d955b1b2a4bae7d22c7287374c14
Budzik <pbudzinski@hotmail.com>
2019-01-07 17:52:58 +0100

    Allow to move shapes on map by mouse

---
+++ Low Entropy/Assets/Scripts/MouseControll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseControll : MonoBehaviour
+{
+    public Rigidbody2D object1;
+    public Rigidbody2D object2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 clickedPlace = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+            Vector2 object2Position = object2.transform.position;
+            Vector2 object1Position = object1.transform.position;
+            object2.AddForce((clickedPlace - object2Position).normalized * 100);
+            object1.AddForce((clickedPlace - object1Position).normalized * 100);
+        }
+    }
+}
---
+++ Low Entropy/Assets/Scripts/WinLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public abstract class WinLevel : MonoBehaviour
+{ 
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(WinCondition())
+        {
+            int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+            if(SceneManager.sceneCount < nextSceneIndex)
+            {
+                SceneManager.LoadScene(nextSceneIndex);
+            }
+        }
+    }
+
+    public abstract bool WinCondition();
+}
